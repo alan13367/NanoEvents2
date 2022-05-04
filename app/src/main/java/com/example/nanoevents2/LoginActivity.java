1f677014cd4d9fcd7355dfe404b30098b7eaf4a4
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2022-05-04 19:06:58 +0200

    API Singleton Initialized

--- app/src/main/java/com/example/nanoevents2/LoginActivity.java
+++ app/src/main/java/com/example/nanoevents2/LoginActivity.java
@@ -8,7 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,24 +23,25 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button logInBtn = findViewById(R.id.signInBtn);
-        logInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(),MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        logInBtn.setOnClickListener(this);
 
         Button signUpBtn = findViewById(R.id.signUpBtn);
         signUpBtn.setPaintFlags(signUpBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG); // underline sign up button
+        signUpBtn.setOnClickListener(this);
+    }
 
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(),SignUpActivity.class);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.signInBtn:
+                Intent intent = new Intent(getBaseContext(),MainActivity.class);
                 startActivity(intent);
-            }
-        });
+                finish();
+                break;
+            case R.id.signUpBtn:
+                Intent intent1 = new Intent(getBaseContext(),SignUpActivity.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/nanoevents2/entities/User.java
@@ -0,0 +1,9 @@
+package com.example.nanoevents2.entities;
+
+public class User {
+    private String name;
+    private String last_name;
+    private String email;
+    private String password;
+    private String image;
+}
